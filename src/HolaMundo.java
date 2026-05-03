7a7f595e1d281f68f071a28d6917ec444d24da5c
Claudia <focxay27@gmail.com>
2026-05-03 17:44:08 +0200

    Primera versión Rama

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,11 +1,15 @@
 public class HolaMundo {
     public static void Main(String[] args) {
         saludar();
+        despedirse();
         /*Claudia Gómez*/
     }
 
+
     private static void saludar() {
         System.out.println("Hola mundo, ¿Qué tal?");
-        System.out.println("Adiós");
+    }
+    private static void despedirse() {
+        System.out.println("Adiós mundo");
     }
 }
